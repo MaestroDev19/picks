9a5fe4c7646087315f56363f4e5a5db4bd77fc0c
charles <charles19rex@gmail.com>
2025-06-03 14:47:17 +0100

    refactor: streamline parameter handling in movie and TV detail pages
    - Simplified the extraction of the `id` parameter in the `generateMetadata` and default export functions for both movie and TV detail pages by directly accessing `params.id`.
    - This change enhances code clarity and consistency across the components, aligning with best practices for maintainability.

--- app/tv/[id]/page.tsx
+++ app/tv/[id]/page.tsx
@@ -1,24 +1,24 @@
 import { getTvDetails } from "@/data/data";
 import { Details } from "@/components/details";
 import { Metadata } from "next";
+
 export async function generateMetadata({
   params,
 }: {
   params: { id: string };
 }): Promise<Metadata> {
-  const { id } = await params;
-  const tv = await getTvDetails(id);
+  const tv = await getTvDetails(params.id);
   return {
     title: `${tv.name} : Picks - Your collection of best tv shows and movies`,
     description: tv.overview,
   };
 }
+
 export default async function TvDetails({
   params,
 }: {
   params: { id: string };
 }) {
-  const { id } = await params;
-  const tv = await getTvDetails(id);
+  const tv = await getTvDetails(params.id);
   return <Details item={tv} />;
 }
